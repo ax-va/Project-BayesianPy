5f2425e09dbdd05cedbcb623445f76015109e6bc
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-09-06 14:32:24 +0200

    Test

--- modeling/factor.py
+++ modeling/factor.py
@@ -2,18 +2,12 @@ from bayesian.modeling.variable import Variable
 
 
 class Factor:
-    def __init__(self, variables, function, link):
+    def __init__(self, variables, function):
         self._variables = variables
         self._function = function
-        self._link = link
 
-    def __call__(self, values):
-        for variable in self._variables:
-            if values[variable.name] not in variable.domain:
-                assert False, f'Value {values[variable.name]} of variable {variable} is not in its domain ' \
-                              f'{variable.domain}'
-        arguments = {self._link[variable_name]: values[variable_name] for variable_name in values.keys()}
-        return self._function(**arguments)
+    def __call__(self, *values):
+        return self._function(*values)
 
 
 if __name__ == '__main__':
@@ -21,10 +15,8 @@ if __name__ == '__main__':
     y = Variable(domain={False, True}) #, name='Y')
     z = Variable(domain={False, True}) #, name='Z')
     f1 = Factor(
-        variables={x, y, z},
+        variables=[x, y, z],
         function=lambda a, b, c: 0.5 if (a or b or c) else 0.1,
-        link={x.name: 'a', y.name: 'b', z.name: 'c'}
     )
-    values = {x.name: True, y.name: False, z.name: True}
-    print(f1(values))
-    print(values)
+    print(f1(True, False, True))
+
