a523ecfcedd348800183049db92ab78bf1749ecb
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-09-07 00:15:00 +0200

    Sum-product algorithm

--- algorithms/sum_product.py
+++ algorithms/sum_product.py
@@ -15,18 +15,20 @@ class SumProduct:
         self._variables = copy.deepcopy(self._factorization.variables)
         # To cache the messages into the dictionary
         self._log_messages = {}
-        # Run until the running parameter is False
+        # Run the algorithm until the running parameter is False
         self._running = True
         # Propagate the messages from the next factors
         self._next_factors = []
         # Propagate the messages from the next variables
         self._next_variables = []
-        # Incoming factors to the variables
-        self._incoming_factors_to_variables = {variable: 0 for variable in self._variables}
-        # Incoming variables to the factors
-        self._incoming_variables_to_factors = {factor: 0 for factor in self._factors}
-        # The query is not yet set
+        # Number of defined incoming factor-variable messages to the variable
+        self._factor_variable_messages_number = {variable: 0 for variable in self._variables}
+        # Number of defined incoming variable-factor messages to the factor
+        self._variable_factor_messages_number = {factor: 0 for factor in self._factors}
+        # Query is not yet set
         self._query = None
+        # Evidence is not given
+        self._evidence = None
 
     def run(self):
         # Startup
@@ -48,10 +50,10 @@ class SumProduct:
             variable, = factor.variables
             # Cache the messages into the dictionary
             self._log_messages[(factor, variable)] = {value: math.log(factor(value)) for value in variable.domain}
-            # Increment the number of incoming factors to the variable
-            self._incoming_factors_to_variables[variable] += 1
-            # If only one factor is left, message passing is possible from the variable
-            if len(variable.factors) - 1 == self._incoming_factors_to_variables[variable]:
+            # Increment the number of incoming factor-variable messages to the variable
+            self._factor_variable_messages_number[variable] += 1
+            # If only one factor-variable message is left, message passing is carried out from the variable
+            if len(variable.factors) - 1 == self._factor_variable_messages_number[variable]:
                 # Propagate a message from this variable to the next factor
                 self._next_variables.append(variable)
         for variable in Factorization.get_variable_leaves(self._variables):
@@ -61,9 +63,9 @@ class SumProduct:
                 factor, = variable.factors
                 # Cache the messages into the dictionary
                 self._log_messages[(variable, factor)] = {value: 0 for value in variable.domain}
-                # Increment the number of incoming variables to the factor
-                self._incoming_variables_to_factors[factor] += 1
-                # If only one variable is left, message passing is possible from the factor
-                if len(factor.variables) - 1 == self._incoming_variables_to_factors[factor]:
+                # Increment the number of incoming variable-factor messages to the factor
+                self._variable_factor_messages_number[factor] += 1
+                # If only one variable-factor message is left, message passing is carried out from the factor
+                if len(factor.variables) - 1 == self._variable_factor_messages_number[factor]:
                     # Propagate a message from this factor to the next variable
                     self._next_factors.append(factor)
