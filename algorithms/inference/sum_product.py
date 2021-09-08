21016e280a253c1593b826d42bb675d6b0916c1e
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-09-08 15:47:32 +0200

    Sum-product algorithm

--- algorithms/inference/sum_product.py
+++ algorithms/inference/sum_product.py
@@ -16,7 +16,7 @@ class SumProduct(Factored):
     a random variable of interest, and E_1 = e_1, ..., E_k = e_k form an evidence, i.e.
     observed values e_1, ..., e_k of random variables E_1, ..., E_k, respectively.
 
-    Attention: only works on trees, only works with categorical distributions.
+    Attention: only works on trees, only works with categorical factors.
     """
     def __init__(self, factorization: Factorization):
         Factored.__init__(self, factorization)
@@ -35,7 +35,7 @@ class SumProduct(Factored):
         self._initialize()
         # Running the main loop
         while self._running:
-            # Stop condition
+            # Check the stop condition
             if len(self._query_variable.passed_neighbors) == len(self._query_variable.factors):
                 # Compute either the marginal or conditional probability distribution
                 # ...
@@ -47,36 +47,37 @@ class SumProduct(Factored):
                 self._from_variables = self._next_variables
                 self._next_variables = []
                 for from_factor in self._from_factors:
-                    pass
-
+                    self._propagate_factor_to_variable_message_not_from_leaf(from_factor)
                 for from_variable in self._from_variables:
-                    # Previous to_variable is current from_variable
-                    to_variable = from_variable
-                    # The log-message from this variable to the only one non-passed factor
-                    variable_to_factor_message = {
-                        value: math.fsum(
-                            message[value] for message in (
-                                self._factor_to_variable_messages[(from_factor, to_variable)]
-                                for from_factor in to_variable.passed_neighbors
-                            )
-                        )
-                        for value in to_variable.domain
-                    }
-                    # The variable-to-factor message to the factor
-                    to_factor, = (factor for factor in from_variable.factors
-                                  if factor not in from_variable.passed_neighbors)
-                    # Cache the log-message into the dictionary
-                    self._variable_to_factor_messages[(from_variable, to_factor)] = variable_to_factor_message
-                    # Append the passed variable-neighbor to the next factor
-                    to_factor.passed_neighbors.append(from_variable)
-                    # If all messages except one are collected,
-                    # then a message can be propagated from the next factor
-                    # to the next variable
-                    self._extend_next_factors(to_factor)
+                    self._propagate_variable_to_factor_message_not_from_leaf(from_variable)
 
     def set_query(self, query: Variable):
         # Variable 'query' of interest for computing P(query) or P(query|evidence)
         self._query_variable = self._variables[self._factorization.variables.index(query)]
+
+    def _create_factor_to_variable_message_from_leaf(self, from_factor, to_variable):
+        if (from_factor, to_variable) not in self._factor_to_variable_messages.keys():
+            # Compute the message
+            factor_to_variable_message = {value: math.log(from_factor(value)) for value in to_variable.domain}
+            # Cache the log-message into the dictionary
+            self._factor_to_variable_messages[(from_factor, to_variable)] = factor_to_variable_message
+
+    def _create_variable_to_factor_message_not_from_leaf(self, from_variable, to_factor):
+        # from_variable was previously to_variable
+        to_variable = from_variable
+        if (from_variable, to_factor) not in self._variable_to_factor_messages.keys():
+            # The log-message from this variable to the only one non-passed factor
+            variable_to_factor_message = {
+                value: math.fsum(
+                    message[value] for message in (
+                        self._factor_to_variable_messages[(from_factor, to_variable)]
+                        for from_factor in to_variable.passed_neighbors
+                    )
+                )
+                for value in to_variable.domain
+            }
+            # Cache the log-message into the dictionary
+            self._variable_to_factor_messages[(from_variable, to_factor)] = variable_to_factor_message
 
     def _extend_next_variables(self, variable):
         # If all messages except one are collected,
@@ -102,41 +103,9 @@ class SumProduct(Factored):
         # Append leer passed neighbors to the factors and variables
         self._initialize_passed_neighbors()
         # Propagation from factor leaves
-        self._initialize_over_factor_leaves()
+        self._propagate_factor_to_variable_messages_from_leaves()
         # Propagation from variable leaves
-        self._initialize_over_variable_leaves()
-
-    def _initialize_over_factor_leaves(self):
-        for from_factor in self.factor_leaves:
-            # The leaf factor has only one variable
-            to_variable = from_factor.variables[0]
-            # Compute the message
-            factor_to_variable_message = {value: math.log(from_factor(value)) for value in to_variable.domain}
-            # Cache the log-message into the dictionary
-            self._factor_to_variable_messages[(from_factor, to_variable)] = factor_to_variable_message
-            # Append the passed factor-neighbor to the variable
-            to_variable.passed_neighbors.append(from_factor)
-            # If all messages except one are collected,
-            # then a message can be propagated from this variable
-            # to the next factor
-            self._extend_next_variables(to_variable)
-
-    def _initialize_over_variable_leaves(self):
-        for from_variable in self.variable_leaves:
-            if from_variable is self._query_variable:
-                continue
-            # The leaf variable has only one factor
-            to_factor = from_variable.factors[0]
-            # Compute the message
-            variable_to_factor_message = {value: 0 for value in from_variable.domain}
-            # Cache the log-message into the dictionary
-            self._variable_to_factor_messages[(from_variable, to_factor)] = variable_to_factor_message
-            # Append the passed variable-neighbor to the factor
-            to_factor.passed_neighbors.append(from_variable)
-            # If all messages except one are collected,
-            # then a message can be propagated from this factor
-            # to the next variable
-            self._extend_next_factors(to_factor)
+        self._propagate_variable_to_factor_messages_from_leaves()
 
     def _initialize_passed_neighbors(self):
         # Append leer passed neighbors to the factors and variables
@@ -144,3 +113,51 @@ class SumProduct(Factored):
             factor.passed_neighbors = []
         for variable in self._variables:
             variable.passed_neighbors = []
+
+    def _propagate_factor_to_variable_message_from_leaf(self, from_factor):
+        # The leaf factor has only one variable
+        to_variable = from_factor.variables[0]
+        self._create_factor_to_variable_message_from_leaf(from_factor, to_variable)
+        # Append the passed factor-neighbor to the variable
+        to_variable.passed_neighbors.append(from_factor)
+        # If all messages except one are collected,
+        # then a message can be propagated from this variable
+        # to the next factor
+        self._extend_next_variables(to_variable)
+
+    def _propagate_factor_to_variable_messages_from_leaves(self):
+        for from_factor in self.factor_leaves:
+            self._propagate_factor_to_variable_message_from_leaf(from_factor)
+
+    def _propagate_variable_to_factor_message_from_leaf(self, from_variable):
+        # The leaf variable has only one factor
+        to_factor = from_variable.factors[0]
+        if (from_variable, to_factor) not in self._variable_to_factor_messages.keys():
+            # Compute the message
+            variable_to_factor_message = {value: 0 for value in from_variable.domain}
+            # Cache the log-message into the dictionary
+            self._variable_to_factor_messages[(from_variable, to_factor)] = variable_to_factor_message
+        # Append the passed variable-neighbor to the factor
+        to_factor.passed_neighbors.append(from_variable)
+        # If all messages except one are collected,
+        # then a message can be propagated from this factor
+        # to the next variable
+        self._extend_next_factors(to_factor)
+
+    def _propagate_variable_to_factor_messages_from_leaves(self):
+        for from_variable in self.variable_leaves:
+            if from_variable is self._query_variable:
+                continue
+            self._propagate_variable_to_factor_message_from_leaf(from_variable)
+
+    def _propagate_variable_to_factor_message_not_from_leaf(self, from_variable):
+        # The variable-to-factor message to the factor
+        to_factor, = (factor for factor in from_variable.factors
+                      if factor not in from_variable.passed_neighbors)
+        self._create_variable_to_factor_message_not_from_leaf(from_variable, to_factor)
+        # Append the passed variable-neighbor to the next factor
+        to_factor.passed_neighbors.append(from_variable)
+        # If all messages except one are collected,
+        # then a message can be propagated from the next factor
+        # to the next variable
+        self._extend_next_factors(to_factor)
