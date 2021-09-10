0838dffd760296569e087c310b71f706b05dc154
Alex Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-09-11 00:36:15 +0200

    Update sum_product.py

--- algorithms/inference/sum_product.py
+++ algorithms/inference/sum_product.py
@@ -23,9 +23,9 @@ class SumProduct(InferenceAlgorithm):
     variable of interest, and E_1 = e_1, ..., E_k = e_k form an evidence, i.e. observed 
     values e_1, ..., e_k of random variables E_1, ..., E_k, respectively.
 
-    Restrictions: only works with categorical factors, only works on trees, leads to dead 
-    lock on loopy graphs.  The factors must have strictly positive values because of the
-    use of logarithms of them.
+    Restrictions: only works with categorical random variables, only works on trees, leads 
+    to dead lock on loopy graphs.  The factors must have strictly positive values because 
+    of the use of logarithms of them.
     
     Recommended: when modeling, reduce the number of random variables in each factor to 
     speed up the inference runtime.
