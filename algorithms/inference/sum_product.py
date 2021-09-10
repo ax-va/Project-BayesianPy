26ab5dc26bef9641a6d7ce8d6c31f72daab0c40e
Alex Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-09-10 20:48:02 +0200

    Update sum_product.py

--- algorithms/inference/sum_product.py
+++ algorithms/inference/sum_product.py
@@ -13,9 +13,9 @@ class SumProduct(InferenceAlgorithm):
     distributions.  That belongs to Message Passing and Variable Elimination Algorithms.
     Here, the factor-to-variable and variable-to-factor messages are propagated from the
     leaves along the tree to the query-variable.  That can be considered as eliminating 
-    the factors and variables in the factor graph.  Instead of messages, the 
-    implementation uses logarithms of messages.  See, for example, "Bayesian Reasoning
-    and Machine Learning" by David Barber, 2012, for more details.
+    the factors and variables in the factor graph.  Instead of the messages, the 
+    implementation uses logarithms of the messages.  See, for example, "Bayesian 
+    Reasoning and Machine Learning" by David Barber, 2012, for more details.
 
     Computes a marginal probability distribution P(Q) or a conditional probability 
     distribution P(Q|E_1 = e_1, ..., E_k = e_k), where Q is a query, i.e. a random 
