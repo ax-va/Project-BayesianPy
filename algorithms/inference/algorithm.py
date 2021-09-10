e5b4e300d63c8e811120604bdb0d275fb5abdb49
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-09-10 15:18:01 +0200

    Sum-product algorithm update

--- algorithms/inference/algorithm.py
+++ algorithms/inference/algorithm.py
@@ -28,8 +28,8 @@ class InferenceAlgorithm:
         return tuple(variable for variable in self._variables if variable.is_non_isolated_leaf())
 
     def set_query(self, query: Variable):
-        # Variable 'query' of interest for computing P(query) or P(query|evidence).
-        # Make sure that the query variable is from the encapsulated sequence in this algorithm.
+        # Variable 'query' of interest for computing P(query) or P(query|evidence)
         if query not in self._factorization.variables:
             raise ValueError('There is no variable in the factorization that corresponds to the query')
+        # Make sure that the query variable is from the encapsulated sequence in this algorithm
         self._query = self._variables[self._factorization.variables.index(query)]
