6d35e1714414876ca7a5d886c143b0345f803c36
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-09-11 22:31:36 +0200

    Sum-Product test

--- algorithms/inference/algorithm.py
+++ algorithms/inference/algorithm.py
@@ -7,17 +7,7 @@ from pyb4ml.models.factor_graphs.model import Model
 
 class InferenceAlgorithm:
     def __init__(self, model: Model):
-        # Save the model
-        self._model = model
-        # Encapsulate the factors and variables inside the algorithm.
-        # New self._factors and self._variables created.
-        self._encapsulate_factors_and_variables()
-        # Query is not yet specified
-        self._query = None
-        # Evidence is not given
-        self._evidence = None
-        # Probability distribution P of interest
-        self._distribution = None
+        self.set_model(model)
 
     @property
     def factor_leaves(self):
@@ -43,6 +33,36 @@ class InferenceAlgorithm:
     def variable_leaves(self):
         return tuple(variable for variable in self._variables if variable.is_non_isolated_leaf())
 
+    def set_query(self, query: Variable):
+        # Variable 'query' of interest for computing P(query) or P(query|evidence)
+        if query not in self._model.variables:
+            raise ValueError('there is no variable in the model that corresponds to the query')
+        # Make sure that the query variable is from the encapsulated sequence in this algorithm
+        self._query = self._variables[self._model.variables.index(query)]
+
+    def print_pd(self):
+        if self._distribution is not None:
+            if self._evidence is None:
+                for value in self._query.domain:
+                    print(f'P({self._query}={value!r})={self.pd(value)}')
+            else:
+                pass
+        else:
+            raise AttributeError('distribution not computed')
+
+    def set_model(self, model):
+        # Save the model
+        self._model = model
+        # Encapsulate the factors and variables inside the algorithm.
+        # New self._factors and self._variables created.
+        self._encapsulate_factors_and_variables()
+        # Query is not yet specified
+        self._query = None
+        # Evidence is not given
+        self._evidence = None
+        # Probability distribution P of interest
+        self._distribution = None
+
     def _create_factor_variables(self, model_factor):
         factor_variables = []
         for model_factor_variable in model_factor.variables:
@@ -65,20 +85,3 @@ class InferenceAlgorithm:
                 name=copy.deepcopy(model_factor.name)
             ) for model_factor in self._model.factors
         )
-
-    def set_query(self, query: Variable):
-        # Variable 'query' of interest for computing P(query) or P(query|evidence)
-        if query not in self._model.variables:
-            raise ValueError('there is no variable in the factorization that corresponds to the query')
-        # Make sure that the query variable is from the encapsulated sequence in this algorithm
-        self._query = self._variables[self._model.variables.index(query)]
-
-    def print_pd(self):
-        if self._distribution is not None:
-            if self._evidence is None:
-                for value in self._query.domain:
-                    print(f'P({self._query}={value!r})={self.pd(value)}')
-            else:
-                pass
-        else:
-            raise AttributeError('distribution not computed')
