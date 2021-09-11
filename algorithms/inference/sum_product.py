a27422100801c5f4a541510b0e55e291a1eeb399
Alex Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-09-11 09:19:56 +0200

    Update sum_product.py

--- algorithms/inference/sum_product.py
+++ algorithms/inference/sum_product.py
@@ -75,8 +75,9 @@ class SumProduct(InferenceAlgorithm):
 
     def get_pd(self):
         """
-        Returns P(Q) or if an evidence is set then P(Q|E_1 = e_1, ..., E_k = e_k)
-        as a function of q, where q is in the domain of random variable Q
+        Returns the probability distribution P(Q) or if an evidence is set then 
+        P(Q|E_1 = e_1, ..., E_k = e_k) as a function of q, where q is in the domain 
+        of random variable Q
         """
         if self._distribution is not None:
             def distribution(value):
