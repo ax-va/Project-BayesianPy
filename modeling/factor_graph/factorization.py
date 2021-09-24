8498bbab9edb67583c60ffa70ea77feefe6c83f7
Alex Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-09-24 23:13:05 +0200

    Update factorization.py

--- modeling/factor_graph/factorization.py
+++ modeling/factor_graph/factorization.py
@@ -2,11 +2,6 @@ class Factorization:
     def __init__(self, factors, variables):
         self._factors = tuple(sorted(set(factors), key=lambda f: f.name))
         self._variables = tuple(sorted(set(variables), key=lambda v: v.name))
-        self._cache = {factor.variables: factor for factor in self._factors}
-
-    @property
-    def cache(self):
-        return self._cache
 
     @property
     def factors(self):
@@ -15,3 +10,6 @@ class Factorization:
     @property
     def variables(self):
         return self._variables
+    
+    def create_factor_cache(self)
+        return {factor.variables: factor for factor in self._factors}
