41bbaf287ab1e28a4189a641d88ca2541ae3b005
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-09-26 20:24:12 +0200

    BEA...

--- algorithms/inference/bucket.py
+++ algorithms/inference/bucket.py
@@ -7,34 +7,35 @@ from pyb4ml.modeling.factor_graph.factor import Factor
 class Bucket:
     def __init__(self, variable):
         self._variable = variable
-        self._factors = []
+        self._input_factors = []
         self._free_variables = []
 
     @property
-    def factors(self):
-        return self._factors
+    def input_factors(self):
+        return self._input_factors
 
     @property
     def variable(self):
         return self._variable
 
     def add_factor(self, factor):
-        self._factors.append(factor)
+        self._input_factors.append(factor)
 
     def add_free_variables(self, variables):
         self._free_variables.extend(variables)
 
     def has_factors(self):
-        return len(self._factors) > 0
+        return len(self._input_factors) > 0
 
     def has_free_variables(self):
         return len(set(self._free_variables)) > 0
 
-    def compute_factor_with_free_variables(self):
+    def compute_output_factor(self):
         # Remove duplicates and sort free variables
         self._set_free_variables()
         # Evaluate free variables
         evaluated_free_variables = FactoredAlgorithm.evaluate_variables(self._free_variables)
+        # Compute the function for the output factor
         function_value_dict = {}
         for free_values in evaluated_free_variables:
             free_variables_with_values = tuple(zip(self._free_variables, free_values))
@@ -43,16 +44,16 @@ class Bucket:
                     factor(
                         *factor.filter_variables_with_values(free_variables_with_values),
                         (self._variable, value)
-                    ) for factor in self._factors
+                    ) for factor in self._input_factors
                 ) for value in self._variable.domain
             )
             function_value_dict[free_values] = factor_value
-        factor_with_free_variables = Factor(
+        output_factor = Factor(
             variables=self._free_variables,
             function=lambda *values: function_value_dict[values],
-            name=f'f_{self._variable}'
+            name='f_' + self._variable.name
         )
-        return factor_with_free_variables
+        return output_factor
 
     def _set_free_variables(self):
         # Remove duplicates and sort free variables
