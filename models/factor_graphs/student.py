64022231c79e7a955d84ffdccc578a3b48defb36
Alex Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-09-27 00:55:21 +0200

    Update student.py

--- models/factor_graphs/student.py
+++ models/factor_graphs/student.py
@@ -12,85 +12,85 @@ class Student(FactorGraph):
     """
     def __init__(self):
         # Random variables
-        self._difficulty = Variable(domain={'d0', 'd1'}, name='Difficulty')
-        self._intelligence = Variable(domain={'i0', 'i1'}, name='Intelligence')
-        self._grade = Variable(domain={'g0', 'g1', 'g2'}, name='Grade')
-        self._sat = Variable(domain={'s0', 's1'}, name='SAT')
-        self._letter = Variable(domain={'l0', 'l1'}, name='Letter')
+        difficulty = Variable(domain={'d0', 'd1'}, name='Difficulty')
+        intelligence = Variable(domain={'i0', 'i1'}, name='Intelligence')
+        grade = Variable(domain={'g0', 'g1', 'g2'}, name='Grade')
+        sat = Variable(domain={'s0', 's1'}, name='SAT')
+        letter = Variable(domain={'l0', 'l1'}, name='Letter')
         variables = {
-            self._difficulty,
-            self._intelligence,
-            self._grade,
-            self._sat,
-            self._letter
+            difficulty,
+            intelligence,
+            grade,
+            sat,
+            letter
         }
 
         # Conditional probability distributions (CPDs)
-        self._cpd_difficulty = {
+        cpd_difficulty = {
             'd0': 0.6,
             'd1': 0.4
         }
-        self._cpd_intelligence = {
+        cpd_intelligence = {
             'i0': 0.7,
             'i1': 0.3
         }
-        self._cpd_grade = {
+        cpd_grade = {
             ('d0', 'i0'): {'g0': 0.30, 'g1': 0.40, 'g2': 0.30},
             ('d0', 'i1'): {'g0': 0.90, 'g1': 0.08, 'g2': 0.02},
             ('d1', 'i0'): {'g0': 0.05, 'g1': 0.25, 'g2': 0.70},
             ('d1', 'i1'): {'g0': 0.50, 'g1': 0.30, 'g2': 0.20}
         }
-        self._cpd_sat = {
+        cpd_sat = {
             'i0': {'s0': 0.95, 's1': 0.05},
             'i1': {'s0': 0.20, 's1': 0.80}
         }
-        self._cpd_letter = {
+        cpd_letter = {
             'g0': {'l0': 0.10, 'l1': 0.90},
             'g1': {'l0': 0.40, 'l1': 0.60},
             'g2': {'l0': 0.99, 'l1': 0.01}
         }
 
         # Factors
-        self._f0 = Factor(
-            variables=(self._difficulty, ),
-            function=lambda x: self._cpd_difficulty[x],
+        f0 = Factor(
+            variables=(difficulty, ),
+            function=lambda x: cpd_difficulty[x],
             name='f0'
         )
-        self._f1 = Factor(
-            variables=(self._intelligence, ),
-            function=lambda x: self._cpd_intelligence[x],
+        f1 = Factor(
+            variables=(intelligence, ),
+            function=lambda x: cpd_intelligence[x],
             name='f1'
         )
-        self._f2 = Factor(
+        f2 = Factor(
             variables=(
-                self._difficulty,
-                self._intelligence,
-                self._grade
+                difficulty,
+                intelligence,
+                grade
             ),
-            function=lambda x, y, z: self._cpd_grade[(x, y)][z],
+            function=lambda x, y, z: cpd_grade[(x, y)][z],
             name='f2'
         )
-        self._f3 = Factor(
+        f3 = Factor(
             variables=(
-                self._intelligence,
-                self._sat
+                intelligence,
+                sat
             ),
-            function=lambda x, y: self._cpd_sat[x][y],
+            function=lambda x, y: cpd_sat[x][y],
             name='f3'
         )
-        self._f4 = Factor(
+        f4 = Factor(
             variables=(
-                self._grade,
-                self._letter
+                grade,
+                letter
             ),
-            function=lambda x, y: self._cpd_letter[x][y],
+            function=lambda x, y: cpd_letter[x][y],
             name='f4'
         )
         factors = {
-            self._f0,
-            self._f1,
-            self._f2,
-            self._f3,
-            self._f4
+            f0,
+            f1,
+            f2,
+            f3,
+            f4
         }
         FactorGraph.__init__(self, factors, variables)
