24924e6764fd12e0cc944c009621c67e40b9431e
Alex Vasiliev <85578981+ax-va@users.noreply.github.com>
2021-10-04 21:39:55 +0200

    Update bucket_elimination.py

--- algorithms/inference/bucket_elimination.py
+++ algorithms/inference/bucket_elimination.py
@@ -10,7 +10,7 @@ class BEA(FactoredAlgorithm):
     """
     The Bucket Elimination Algorithm (BEA) works on factor graphs for random variables
     with categorical probability distributions.  That belongs to Variable Elimination
-    Algorithms.  Here, a bucket contains factors used to eliminate a variable summing 
+    Algorithms.  Here, a bucket contains factors used to eliminate a variable by summing 
     the product of factors over that variable.  Due to that, a new factor is created and 
     moved into one remaining bucket.  That is repeated until the query buckets contain 
     factors that depend only on the query variables.  Instead of the factors, the 
